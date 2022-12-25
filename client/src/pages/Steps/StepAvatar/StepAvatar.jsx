efbd1cc428e9968cb9c6304c2ab961bf63b94d9a
Shrey1012 <shrey9157@gmail.com>
2022-12-25 22:58:13 +0530

    Added name and avatar page

--- client/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ client/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import styles from './StepAvatar.module.css'
+import React, { useState } from "react";
+import styles from "./StepAvatar.module.css";
+import Card from "../../../components/shared/Card/Card";
+import Button from "../../../components/shared/Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { setAvatar } from "../../../store/activateSlice";
+import {setAuth} from "../../../store/authSlice";
+import { activate } from "../../../http";
 
-const StepAvatar = ({onNext}) => {
+const StepAvatar = ({ onNext }) => {
+  const dispatch = useDispatch();
+  const { name,avatar } = useSelector((state) => state.activate);
+  const [image, setImage] = useState("./images/monkey-avatar.png");
+  const captureImage = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImage(reader.result);
+      dispatch(setAvatar(reader.result));
+    };
+  };
+  const submit = async () => {
+    try {
+      const {data} = await activate({name,avatar});
+      if (data.auth){
+        dispatch(setAuth(data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-    <div>StepAvatar</div>
-    <button onClick={onNext}>Next</button>
+      <Card title={`Okay, ${name}`} icon="monkey-emoji">
+        <p className={styles.subHeading}>How’s this photo?</p>
+        <div className={styles.avatarWrapper}>
+          <img src={image} className={styles.avatarImage} alt="avatar" />
+        </div>
+        <div>
+          <input
+            id="avatarInput"
+            type="file"
+            className={styles.avatarInput}
+            onChange={captureImage}
+          />
+          <label className={styles.avatarLabel} htmlFor="avatarInput">
+            Choose a different photo
+          </label>
+        </div>
+        <div>
+          <Button onClick={submit} text="Next" />
+        </div>
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default StepAvatar
+export default StepAvatar;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const router = require('./routes');
-require('dotenv').config();
-const DbConnect = require('./database');
-const cors = require('cors');
+const express = require("express");
+const router = require("./routes");
+require("dotenv").config();
+const DbConnect = require("./database");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(cookieParser());
+
 const corsOption = {
-    origin: ['http://localhost:3000']
+  credentials: true,
+  origin: ["http://localhost:3000"],
 };
 app.use(cors(corsOption));
 
+app.use('/storage', express.static('storage'));
+
 const PORT = process.env.PORT || 5000;
 DbConnect();
-app.use(express.json());
+app.use(express.json({ limit: "8mb" }));
 
 app.use(router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
